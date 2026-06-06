dd9c80ed6623655e7097cf3150d35b93cc6eb537
vini-bgs <viniciustheros@gmail.com>
2026-06-06 20:33:41 -0300

    feat: segundo commit

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -3,19 +3,32 @@ from pathlib import Path
 from pipeline.logger import logger
 
 
-path: Path = Path("data/raw/csat.csv")
+def csv_para_df(caminho_arquivo: Path) -> pd.DataFrame:
+    """
+    Função que lê um arquivo .csv e retorna um DataFrame
+    """
+    logger.info("Iniciando a extração...")
+    if not caminho_arquivo.exists():
+        logger.error(f"Arquivo não encontrado: {caminho_arquivo}")
+        raise FileNotFoundError(f"Arquivo não encontrado: {caminho_arquivo}")
 
-if not path.exists():
-    logger.error(f"Arquivo não encontrado: {path}")
-    raise FileNotFoundError(f"Arquivo não encontrado: {path}")
+    df: pd.DataFrame = pd.read_csv(
+        caminho_arquivo,
+        sep=";",
+        encoding="utf-8",
+        dtype=str,
+        low_memory=False,
+        keep_default_na=False,
+    )
 
-df: pd.DataFrame = pd.read_csv(
-    path,
-    sep=";",
-    encoding="utf-8",
-    dtype=str,  # tipos de dados de cada coluna serão tratadps no transfor.py
-    low_memory=False,
-    keep_default_na=False,
-)
+    logger.info(f"{len(df)} linhas carregadas de '{caminho_arquivo.name}'")
 
-logger.info(f"{len(df)} linhas carregadas de '{path.name}")
+    return df
+
+
+if __name__ == "__main__":
+    try:
+        path: Path = Path("data/raw/filed.csv")
+        print(csv_para_df(path))
+    except Exception:
+        print("err")
