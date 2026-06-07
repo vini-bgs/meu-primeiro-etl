55777b162893905ad2aa7526c663bd9cb7eda10f
vini-bgs <viniciustheros@gmail.com>
2026-06-06 21:29:58 -0300

    🔄 atualização do função renomea_colunas

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,24 +1,15 @@
 import pandas as pd
-from pipeline.extract import csv_para_df
 from pipeline.logger import logger
 from pathlib import Path
 
 
-def renomea_colunas(caminho: Path) -> list:
-    df: pd.DataFrame = csv_para_df(caminho)
-
-    if df.empty():
-        logger.erro(f"DataFrame vazio: {caminho.name}")
-        raise ValueError(f"DataFrame vazio: {caminho.name}")
-
-    colunas: list = df.columns
-
-    nova_lista_colunas: list = list()
-    for c in colunas:
-        nome_coluna_tratado: str = c.lower().strip().replace(" ", "_")
-        nova_lista_colunas.append(nome_coluna_tratado)
-
-    return nova_lista_colunas
+def renomea_colunas(df: pd.DataFrame) -> pd.DataFrame:
+    try:
+        df.columns = [c.lower().strip().replace(" ", "_") for c in df.columns]
+    except Exception as err:
+        logger.error(f"Atenção! {err}")
+        raise Exception((f"Atenção! {err}"))
+    return df
 
 
 if __name__ == "__main__":
